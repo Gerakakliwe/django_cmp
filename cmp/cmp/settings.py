d52665a5ca8509e0c64b1926414e6e4bcf4c79c3
WxyWgYPU <bogdan.bondarenko99@gmail.com>
2021-08-17 16:55:10 +0300

    Add reset password functionality

--- cmp/cmp/settings.py
+++ cmp/cmp/settings.py
@@ -133,3 +133,13 @@ MEDIA_ROOT = BASE_DIR/"static/images"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "your mail here"
+EMAIL_HOST_PASSWORD = "your password here"
