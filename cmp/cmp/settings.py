6847010664525f1c83e67630702633b1b9fbc844
WxyWgYPU <bogdan.bondarenko99@gmail.com>
2021-08-19 23:28:28 +0300

    Add a bit of styling to password reseting pages

--- cmp/cmp/settings.py
+++ cmp/cmp/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "your mail here"
+EMAIL_HOST_USER = "your email here"
 EMAIL_HOST_PASSWORD = "your password here"
